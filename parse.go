6b44bafc8cdf3aecec8c0d5adee6a4b64f79b8ba
Nelson Elhage <nelhage@nelhage.com>
2018-12-31 18:44:17 +0000

    parse sections

--- parse.go
+++ parse.go
@@ -9,12 +9,39 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
-var PAT = regexp.MustCompile(`(\d{1,2}/\d{1,2})\s*&?\s*(.*)   \s+((-\s*)?[0-9,]*\.\d+)`)
+var (
+	TxnPat     = regexp.MustCompile(`(\d{1,2}/\d{1,2})\s*&?\s*(.*)   \s+((-\s*)?[0-9,]*\.\d+)`)
+	SectionPat = regexp.MustCompile(`^\s*(PAYMENTS AND OTHER CREDITS|PURCHASE|FEES CHARGED)`)
+)
 
-func readOne(fh io.Reader) {
-	var pos, neg int64
+type rawTxn struct {
+	category   string
+	date       string
+	descriptor string
+	amount     string
+}
+
+func formatCents(amt int64) string {
+	signum := '+'
+	if amt < 0 {
+		signum = '-'
+		amt = -amt
+	}
+	return fmt.Sprintf("%c%d.%02d", signum, amt/100, amt%100)
+}
+
+func processOne(path string) {
+	fh, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("open(%q): %v", path, err)
+	}
+	defer fh.Close()
+
+	var raw []rawTxn
+	var section string
 
 	r := bufio.NewReader(fh)
 	for {
@@ -25,38 +52,43 @@ func readOne(fh io.Reader) {
 			}
 			log.Fatalf("read: %v", err)
 		}
-		matches := PAT.FindSubmatch(line)
+		sectMatch := SectionPat.FindSubmatch(line)
+		if sectMatch != nil {
+			section = string(sectMatch[1])
+		}
+		matches := TxnPat.FindSubmatch(line)
 		if matches == nil {
 			continue
 		}
-		fmt.Printf("%s,%s,%s\n", matches[1], bytes.TrimRight(matches[2], " "), matches[3])
+		// fmt.Printf("%s,%s,%s\n", matches[1], bytes.TrimRight(matches[2], " "), matches[3])
+		raw = append(raw, rawTxn{
+			section,
+			string(matches[1]),
+			string(bytes.TrimRight(matches[2], " ")),
+			string(matches[3]),
+		})
+	}
 
-		amt := matches[3]
-		amt = bytes.Replace(amt, []byte(","), []byte{}, -1)
-		amt = bytes.Replace(amt, []byte("."), []byte{}, -1)
-		cents, err := strconv.ParseInt(string(amt), 10, 64)
+	totals := make(map[string]int64)
+	for _, txn := range raw {
+		amt := txn.amount
+		amt = strings.Replace(amt, ",", "", -1)
+		amt = strings.Replace(amt, ".", "", -1)
+		cents, err := strconv.ParseInt(amt, 10, 64)
 		if err != nil {
 			fmt.Printf("parse(%s): %v", amt, err)
-		} else {
-			if cents > 0 {
-				pos += cents
-			} else {
-				neg -= cents
-			}
+			continue
 		}
+		totals[txn.category] += cents
 	}
-	fmt.Printf("TOTAL: +%d.%d -%d.%d\n", pos/100, pos%100, neg/100, neg%100)
+	fmt.Printf("TOTALS -- %s\n", path)
+	for cat, amt := range totals {
+		fmt.Printf("%30s %s\n", cat, formatCents(amt))
+	}
 }
 
 func main() {
-	for _, path := range os.Args {
-		fh, e := os.Open(path)
-		if e != nil {
-			log.Fatalf("open(%q): %v", path, e)
-		}
-		defer fh.Close()
-		readOne(fh)
+	for _, path := range os.Args[1:] {
+		processOne(path)
 	}
 }
-
-// perl -lane 'print "$1,$2,$3" if m {(\d{1,2}/\d{1,2})\s*&?\s*(.*)   \s+(\d+\.\d+)}' 20180117-statements-4997-.txt  | less
